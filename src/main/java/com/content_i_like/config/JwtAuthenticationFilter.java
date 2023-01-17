81c7c0667fa24b9e3a9d40b8f62bdd9417bdd218
chordpli <chordplaylist@gmail.com>
2023-01-17 23:34:11 +0900

    feat($config): add jwtfilter
    1. Spring Security 설정을 위해 임시적으로 생성한 JwtFilter이므로 구현이 더 필요합니다.
    2. 현재 header의 authorizaion을 받아오는 로직, header에서 받아온 authorizaion을 잘라 token을 분리하는 로직만 구현된 상태.

---
+++ src/main/java/com/content_i_like/config/JwtAuthenticationFilter.java
@@ -0,0 +1,39 @@
+package com.content_i_like.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    //private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String memberEmail;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+        // memberEmail = // todo extract the userEmail from JWT token;
+
+    }
+}
