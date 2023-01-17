f1cadd5d804d95523548e8b612366ce753c01d1a
lay423 <junpang486@gmail.com>
2023-01-17 10:25:09 +0900

    ✔ 프로젝트 빌드

---
+++ src/main/java/com/content_i_like/ContentILikeApplication.java
@@ -0,0 +1,13 @@
+package com.content_i_like;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentILikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentILikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/content_i_like/ContentILikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.content_i_like;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentILikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
