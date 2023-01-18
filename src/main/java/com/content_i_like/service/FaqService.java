e9bb9d5ac1227f43bb431996cc858d71e57128fb
lay423 <junpang486@gmail.com>
2023-01-18 14:09:05 +0900

    🚨 fix: 누락된 @Service 추가

--- src/main/java/com/content_i_like/service/FaqService.java
+++ src/main/java/com/content_i_like/service/FaqService.java
@@ -6,7 +6,9 @@ import com.content_i_like.repository.FAQRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
+@Service
 @RequiredArgsConstructor
 public class FaqService {
 
