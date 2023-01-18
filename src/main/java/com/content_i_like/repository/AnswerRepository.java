f532a1e90d1fdfa7648192f6e402299b241b136a
lay423 <junpang486@gmail.com>
2023-01-18 14:07:44 +0900

    ✨feat: add Answer entity

---
+++ src/main/java/com/content_i_like/repository/AnswerRepository.java
@@ -0,0 +1,6 @@
+package com.content_i_like.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<AnswerRepository, Long> {
+}
