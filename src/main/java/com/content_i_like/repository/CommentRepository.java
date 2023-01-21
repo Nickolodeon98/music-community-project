11588136d58548b7d8f3901adb86dc64c37b1043
chordpli <chordplaylist@gmail.com>
2023-01-21 22:50:59 +0900

    ✨feat($api): feature get read comment detail api

--- src/main/java/com/content_i_like/repository/CommentRepository.java
+++ src/main/java/com/content_i_like/repository/CommentRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Modifying(clearAutomatically = true)
     @Query("update Comment c set c.commentContent = :content, c.lastModifiedAt = current_timestamp where c.commentNo = :commentNo ")
     void update(@Param("content") String commentContent,@Param("commentNo") Long commentNo);
+
+    Optional<Comment> findCommentByRecommend_RecommendNoAndCommentNo(Long recommendNo, Long commentNo);
 }
