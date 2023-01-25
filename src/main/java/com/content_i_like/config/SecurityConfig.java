3c8e4260979e05975d766cd4e84b15f8535596c3
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-25 10:03:50 +0900

    👷 refactor: add a new url that is permitted so that spotify web api test is enabled

--- src/main/java/com/content_i_like/config/SecurityConfig.java
+++ src/main/java/com/content_i_like/config/SecurityConfig.java
@@ -22,7 +22,8 @@ public class SecurityConfig {
     private final OAuthService oAuthService;
 
     private final String[] AUTHORIZATION = {"/api/v1/**", ""};
-    private final String[] TEST_URL = {"/api/v1/hello/**", "/api/v1/member/join", "/api/v1/member/login", "/api/v1/member/passwd/find_pw"};
+    private final String[] TEST_URL = {"/api/v1/hello/**", "/api/v1/member/join", "/api/v1/member/login", "/api/v1/member/passwd/find_pw"
+    ,"/api/v1/test/**"};
 
     private final String[] SWAGGER = {"/v3/api-docs/**", "/swagger-ui/**"};
 
