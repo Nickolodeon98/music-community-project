e3416f2bdbc541b8d969ba581566dbaad440ec0f
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-25 23:31:29 +0900

    ✨feat: add new controller and its test for apis related to music

---
+++ src/main/java/com/content_i_like/controller/MusicRestController.java
@@ -0,0 +1,9 @@
+package com.content_i_like.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MusicRestController {
+
+
+}
---
+++ src/test/java/com/content_i_like/controller/MusicRestControllerTest.java
@@ -0,0 +1,7 @@
+package com.content_i_like.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MusicRestControllerTest {
+
+}
