74f0fc8e474acde2b9913a61e4348e662b783bf9
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-26 11:18:48 +0900

    ✨feat: add a method that checks whether a given user credential is stored in Member table

--- src/main/java/com/content_i_like/service/MusicService.java
+++ src/main/java/com/content_i_like/service/MusicService.java
@@ -1,7 +1,11 @@
 package com.content_i_like.service;
 
 import com.content_i_like.domain.dto.tracks.TrackGetResponse;
+import com.content_i_like.domain.entity.Member;
 import com.content_i_like.domain.entity.Song;
+import com.content_i_like.exception.ContentILikeAppException;
+import com.content_i_like.exception.ErrorCode;
+import com.content_i_like.repository.MemberRepository;
 import com.content_i_like.repository.SongRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -13,8 +17,16 @@ import org.springframework.stereotype.Service;
 public class MusicService {
 
     private final SongRepository songRepository;
+    private final MemberRepository memberRepository;
 
-    public Page<TrackGetResponse> getEveryTrack(Pageable pageable) {
+    public Member validateMember(String userEmail) {
+        return memberRepository.findByEmail(userEmail)
+                .orElseThrow(()->new ContentILikeAppException(ErrorCode.NOT_FOUND, ErrorCode.NOT_FOUND.getMessage()));
+    }
+
+    public Page<TrackGetResponse> getEveryTrack(Pageable pageable, String userEmail) {
+        Member member = validateMember(userEmail);
+
         Page<Song> tracks = songRepository.findAll(pageable);
 
         return tracks.map(TrackGetResponse::of);
