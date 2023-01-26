091ea87258e464827c3d4d2254d3542481ce2e95
Nickolodeon98 <sjeon0730@gmail.com>
2023-01-26 11:01:50 +0900

    ✨feat: implement API to fetch and show every track data from DB

--- src/main/java/com/content_i_like/controller/MusicRestController.java
+++ src/main/java/com/content_i_like/controller/MusicRestController.java
@@ -1,9 +1,30 @@
 package com.content_i_like.controller;
 
+import com.content_i_like.domain.Response;
+import com.content_i_like.domain.dto.tracks.TrackGetResponse;
+import com.content_i_like.service.MusicService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/music")
+@RequiredArgsConstructor
 public class MusicRestController {
 
+    private final MusicService musicService;
+    @GetMapping("/all")
+    public Response<Page<TrackGetResponse>> showAllTracks(
+            @PageableDefault(sort="songNo", direction = Sort.Direction.DESC) final Pageable pageable) {
+
+        Page<TrackGetResponse> tracks = musicService.getEveryTrack(pageable);
+
+        return Response.success(tracks);
+    }
 
 }
