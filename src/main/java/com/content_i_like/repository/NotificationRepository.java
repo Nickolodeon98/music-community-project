245d4ba05bd5ba5917af371e17421a0f818804d3
seanee3670 <cseok3670@gmail.com>
2023-01-27 20:05:36 +0900

    👷refactor: change method type (List -> Slice)

--- src/main/java/com/content_i_like/repository/NotificationRepository.java
+++ src/main/java/com/content_i_like/repository/NotificationRepository.java
@@ -4,12 +4,11 @@ import com.content_i_like.domain.entity.Member;
 import com.content_i_like.domain.entity.Notification;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
-    Page<Notification> findAllByMember(Member member, Pageable pageable);
+    Page<Notification> findPageAllByMember(Member member, Pageable pageable);
 
-    List<Notification> findAllByMember(Member member);
+    Slice<Notification> findSliceAllByMember(Member member, Pageable pageable);
 }
