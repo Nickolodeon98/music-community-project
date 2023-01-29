7b0cff2b5ad94bd6feca832d58894bf90ed651ab
lucyoz <lucyoz.kwon@gmail.com>
2023-01-29 20:05:00 +0900

    ✨feat: setErrorResponse related in jwtException

--- src/main/java/com/content_i_like/exception/ErrorCode.java
+++ src/main/java/com/content_i_like/exception/ErrorCode.java
@@ -16,7 +16,11 @@ public enum ErrorCode {
   INCONSISTENT_INFORMATION(HttpStatus.CONFLICT, "일치하지 않는 정보입니다."),
   REJECT_PASSWORD(HttpStatus.CONFLICT, "비밀번호는 8~16자입니다."),
   UNKNOWN_ERROR(HttpStatus.BAD_REQUEST, "알 수 없는 에러가 발생했습니다."),
-  DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB에러");
+  DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "DB에러"),
+
+  EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+  INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
+  INVALID_PERMISSION(HttpStatus.UNAUTHORIZED, "사용자가 권한이 없습니다.");
 
   private HttpStatus status;
   private String message;
