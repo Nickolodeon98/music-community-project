9c8d22208fad6933080ce469849cdfa39f298129
Nickolodeon98 <sjeon0730@gmail.com>
2023-02-02 09:22:29 +0900

    ✨feat: add of method that converts from entity to DTO

--- src/main/java/com/content_i_like/domain/dto/albums/AlbumGetResponse.java
+++ src/main/java/com/content_i_like/domain/dto/albums/AlbumGetResponse.java
@@ -1,5 +1,10 @@
 package com.content_i_like.domain.dto.albums;
 
+import com.content_i_like.domain.entity.Album;
+
 public class AlbumGetResponse {
 
+  public static AlbumGetResponse of(Album album) {
+
+  }
 }
