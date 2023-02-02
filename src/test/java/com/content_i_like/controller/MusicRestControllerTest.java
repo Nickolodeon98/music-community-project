2c646ddce9c39e01ea297706e57c8d0b3b9cb0fb
Nickolodeon98 <sjeon0730@gmail.com>
2023-02-02 13:20:49 +0900

    ✨feat: add a test that verifies whether fetching data of a single track succeeds

---
+++ src/test/java/com/content_i_like/controller/MusicRestControllerTest.java
@@ -0,0 +1,61 @@
+package com.content_i_like.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.content_i_like.domain.dto.tracks.TrackGetResponse;
+import com.content_i_like.fixture.Fixture;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WithMockUser
+@WebMvcTest(MusicRestController.class)
+class MusicRestControllerTest {
+
+  @MockBean
+  MusicService musicService;
+
+  @Autowired
+  MockMvc mockMvc;
+
+  final String BASE_URL = "/api/v1/music/";
+  TrackGetResponse track;
+
+  final Long TRACK_ID = 1L;
+
+  @BeforeEach
+  void setUp() {
+    track = TrackGetResponse.of(Fixture.getTrackFixture(Fixture.getAlbumFixture(Fixture.getArtistFixture())));
+    track.setThumbnailUrl("https://i.scdn.co/image/ab67616d00001e021abe22e6883e5a8b3f4726e2");
+  }
+
+  @Nested
+  @DisplayName("음원 한개 상세 조회")
+  class DetailedTrackInfo {
+    @Test
+    @DisplayName("성공")
+    void success_specify_a_track() throws Exception {
+      given(musicService.getASingleTrackInfo(TRACK_ID)).willReturn(track);
+
+      mockMvc.perform(get(BASE_URL + "track/" + TRACK_ID).with(csrf()))
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.resultCode").value("SUCCESS"))
+          .andDo(print());
+
+      verify(musicService).getASingleTrackInfo(TRACK_ID);
+    }
+  }
+}
