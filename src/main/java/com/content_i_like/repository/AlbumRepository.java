43a14a4f0c05d2a11686e070cda2466579da87b7
Nickolodeon98 <sjeon0730@gmail.com>
2023-02-02 09:23:25 +0900

    ✨feat: add method that uses queries to find entities with column title values containing certain keyword

--- src/main/java/com/content_i_like/repository/AlbumRepository.java
+++ src/main/java/com/content_i_like/repository/AlbumRepository.java
@@ -1,8 +1,12 @@
 package com.content_i_like.repository;
 
 import com.content_i_like.domain.entity.Album;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlbumRepository extends JpaRepository<Album, Long> {
 
+  Optional<Page<Album>> findAllByAlbumTitleContaining(String keyword, Pageable pageable);
 }
