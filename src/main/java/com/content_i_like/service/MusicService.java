4b497d1dbf2849334850933f67f37513a4e2eae8
Nickolodeon98 <sjeon0730@gmail.com>
2023-02-02 13:33:17 +0900

    ✨feat: complete a method in MusicService that returns track info POJO to be converted into Json format data in response of requested track id column value

--- src/main/java/com/content_i_like/service/MusicService.java
+++ src/main/java/com/content_i_like/service/MusicService.java
@@ -1,13 +1,24 @@
 package com.content_i_like.service;
 
 import com.content_i_like.domain.dto.tracks.TrackGetResponse;
+import com.content_i_like.domain.entity.Track;
+import com.content_i_like.exception.ContentILikeAppException;
+import com.content_i_like.exception.ErrorCode;
+import com.content_i_like.repository.TrackRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MusicService {
 
+  private final TrackRepository trackRepository;
 
   public TrackGetResponse getASingleTrackInfo(Long trackNo) {
+    Track track = trackRepository.findById(trackNo)
+        .orElseThrow(()->new ContentILikeAppException(ErrorCode.NOT_FOUND, ErrorCode.NOT_FOUND.getMessage()));
 
+    return TrackGetResponse.of(track);
   }
 }
