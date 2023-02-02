5ec4c2982e6b020593fe49cd931c21cb654f0f34
Nickolodeon98 <sjeon0730@gmail.com>
2023-02-02 09:33:19 +0900

    ✨feat: change method that finds every entity with certain keyword contained to return not Optional type value

--- src/main/java/com/content_i_like/repository/AlbumRepository.java
+++ src/main/java/com/content_i_like/repository/AlbumRepository.java
@@ -1,6 +1,7 @@
 package com.content_i_like.repository;
 
 import com.content_i_like.domain.entity.Album;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,5 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AlbumRepository extends JpaRepository<Album, Long> {
 
-  Optional<Page<Album>> findAllByAlbumTitleContaining(String keyword, Pageable pageable);
+  List<Album> findAllByAlbumTitleContaining(String keyword);
 }
