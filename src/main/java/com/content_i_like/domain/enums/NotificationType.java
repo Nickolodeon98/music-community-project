a76b7f300d34c36dd81a97f7f1697f587f1ffebd
seanee3670 <cseok3670@gmail.com>
2023-02-03 14:36:01 +0900

    ✨feat: add POINT_WELCOME type for giving welcome points to new members

--- src/main/java/com/content_i_like/domain/enums/NotificationType.java
+++ src/main/java/com/content_i_like/domain/enums/NotificationType.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 public enum NotificationType {
   POINT_ADJUSTMENT("포인트가 정산되었습니다"),
+  POINT_WELCOME("회원가입을 축하드리며 소정의 포인트를 발급해드렸습니다."),
   LIKES("좋아요가 눌렸습니다."),
   COMMENTS("댓글이 달렸습니다.");
 
