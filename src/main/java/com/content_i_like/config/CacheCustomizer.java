3c12045fc0b2c0f6c07da532d61a9219e203973d
Nickolodeon98 <sjeon0730@gmail.com>
2023-02-03 09:18:21 +0900

    ✨feat: add configuration class for caching as well as customizing cache names

---
+++ src/main/java/com/content_i_like/config/CacheCustomizer.java
@@ -0,0 +1,15 @@
+package com.content_i_like.config;
+
+import java.util.List;
+import org.springframework.boot.autoconfigure.cache.CacheManagerCustomizer;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CacheCustomizer implements CacheManagerCustomizer<ConcurrentMapCacheManager> {
+
+  @Override
+  public void customize(ConcurrentMapCacheManager cacheManager) {
+    cacheManager.setCacheNames(List.of("transactions"));
+  }
+}
