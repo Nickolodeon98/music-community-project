d844725008dedc821e12420143e3a029cc9e1483
seanee3670 <cseok3670@gmail.com>
2023-02-05 23:11:03 +0900

    🚨fix: change request mapping url to "/api/v1/notifications" from "/api/v1/recommends"

--- src/main/java/com/content_i_like/controller/NotificationRestController.java
+++ src/main/java/com/content_i_like/controller/NotificationRestController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api/v1/recommends")
+@RequestMapping("/api/v1/notifications")
 @RequiredArgsConstructor
 public class NotificationRestController {
 
