7eb7d64d050b19c991a9f80067b8254b135805ca
lay423 <junpang486@gmail.com>
2023-02-06 13:06:37 +0900

    🚨fix: share html problem

--- src/main/java/com/content_i_like/controller/FaqController.java
+++ src/main/java/com/content_i_like/controller/FaqController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,35 +18,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/faq")
 public class FaqController {
 
   private final FaqService faqService;
 
-  @GetMapping()
-  public Response<Page<FaqResponse>> getAllFaq(Pageable pageable) {
-    return Response.success(faqService.getAllFaq(pageable));
+  @RequestMapping("faq/test")
+  public String test() {
+    return "test";
   }
 
-  @GetMapping("/{category}")
-  public Response<Page<FaqResponse>> getFaqByCategory(Pageable pageable,
-      @PathVariable String category) {
-    return Response.success(faqService.getFaqByCategory(pageable, category));
+  @RequestMapping("testFaq")
+  public String test1() {
+    return "test";
   }
 
-  @GetMapping("/searches/{keyWord}")
-  public Response<Page<FaqResponse>> getFaqByKeyWord(Pageable pageable,
-      @PathVariable String keyWord) {
-    return Response.success(faqService.getFaqByKeyWord(pageable, keyWord));
+
+  @GetMapping("faq")
+  public String getAllFaq(Pageable pageable) {
+    Page<FaqResponse> allFaq = faqService.getAllFaq(pageable);
+
+    return null;
   }
 
-  @GetMapping("/details/{faqNo}")
-  public Response<FaqDetailsResponse> getFaqDetails(@PathVariable Long faqNo) {
-    return Response.success(faqService.getFaqDetails(faqNo));
-  }
-
-  @PostMapping()
-  public Response<FaqResponse> addFaq(@RequestBody FaqRequest faqRequest) {
-    return Response.success(faqService.addFaq(faqRequest));
-  }
+//  @GetMapping("/{category}")
+//  public Response<Page<FaqResponse>> getFaqByCategory(Pageable pageable,
+//      @PathVariable String category, Model model) {
+//    return null;
+//  }
+//
+//  @GetMapping("/searches/{keyWord}")
+//  public Response<Page<FaqResponse>> getFaqByKeyWord(Pageable pageable,
+//      @PathVariable String keyWord) {
+//    return Response.success(faqService.getFaqByKeyWord(pageable, keyWord));
+//  }
+//
+//  @GetMapping("/details/{faqNo}")
+//  public Response<FaqDetailsResponse> getFaqDetails(@PathVariable Long faqNo) {
+//    return Response.success(faqService.getFaqDetails(faqNo));
+//  }
+//
+//  @PostMapping()
+//  public Response<FaqResponse> addFaq(@RequestBody FaqRequest faqRequest) {
+//    return Response.success(faqService.addFaq(faqRequest));
+//  }
 }
