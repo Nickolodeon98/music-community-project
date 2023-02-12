45fa8708f740544dd9284f5b06ba6a41456af1cc
lucyoz <lucyoz.kwon@gmail.com>
2023-02-12 14:44:36 +0900

    ✨feat($front): add change-pw page

--- src/main/resources/static/js/member.js
+++ src/main/resources/static/js/member.js
@@ -7,20 +7,31 @@ function checkAll(){
        }
  }
 
-function test() {
-	var p1 = document.getElementById('password1').value;
-	var p2 = document.getElementById('password2').value;
+function checkPw() {
+	var p1 = document.getElementById('password').value;
+	var num = p1.search(/[0-9]/g);
+	var eng = p1.search(/[a-z]/ig);
+	var spe = p1.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+	var p2 = document.getElementById('verification').value;
 
-	if(p1.length < 6) {
-			alert('입력한 글자가 6글자 이상이어야 합니다.');
+	if(p1.length < 8 || p1.length > 20) {
+			alert('비밀번호는 8~20글자여야합니다.');
 			return false;
 		}
-
+	if(p1.search(/\s/)!=-1){
+		alert('비밀번호는 공백없이 입력해주세요.');
+		return false;
+	}
+	if(num < 0 || eng < 0 || spe < 0 ){
+		alert("영문,숫자, 특수문자를 혼합한 8~20글자를 입력해주세요.");
+		return false;
+	}
 		if( p1 != p2 ) {
-		  alert("비밀번호불일치");
-		  return false;
-		} else{
-		  alert("비밀번호가 일치합니다");
-		  return true;
-		}
+			alert("비밀번호가 일치하지 않습니다.");
+			return false;
+		  } else{
+			alert("비밀번호가 변경되었습니다.");
+			return true;
+		  }
+
 }
