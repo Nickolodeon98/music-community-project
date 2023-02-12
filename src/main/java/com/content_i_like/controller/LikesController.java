5c58407737c71ae49467dfb060ff0310b210c9e3
chordpli <chordplaylist@gmail.com>
2023-02-12 16:06:44 +0900

    ✨feat($front): 좋아요 구현
    1. 로그인 구현
    2. ajax로 비동기 좋아요 구현

--- src/main/java/com/content_i_like/controller/LikesController.java
+++ src/main/java/com/content_i_like/controller/LikesController.java
@@ -1,15 +1,23 @@
 package com.content_i_like.controller;
 
 import com.content_i_like.domain.Response;
+import com.content_i_like.domain.dto.member.MemberLoginResponse;
+import com.content_i_like.repository.MemberRepository;
 import com.content_i_like.service.LikesService;
+import com.content_i_like.service.MemberService;
+import com.content_i_like.service.ValidateService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,21 +26,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LikesController {
 
   private final LikesService likesService;
+  private final MemberService memberService;
+  private final ValidateService validateService;
 
   /**
    * 좋아요를 활성화 또는 비활성화 합니다.
    *
-   * @param authentication header에서 받아오는 token.
-   * @param recommendNo    요청을 받는 recommend글의 no.
+   * @param recommendNo 요청을 받는 recommend글의 no.
    * @return 좋아요 활성 또는 비활성 상태의 결과 반환
    */
   @PostMapping("/{recommendNo}/likes")
-  public Response<String> changeLikesStatus(final Authentication authentication,
-      @PathVariable final Long recommendNo) {
-    log.info("authentication = {}, recommendNo = {}", authentication, recommendNo);
+  @ResponseBody
+  public Long changeLikesStatus(@PathVariable final Long recommendNo,
+      HttpServletRequest servletRequest) {
+    HttpSession session = servletRequest.getSession(false);
+    MemberLoginResponse loginResponse = (MemberLoginResponse) session.getAttribute("loginUser");
+    String memberEmail = validateService.validateMemberByMemberNo(loginResponse.getMemberNo()).getEmail();
+    likesService.changeLikesStatus(memberEmail, recommendNo);
 
-    String userName = authentication.getName();
-    return Response.success(likesService.changeLikesStatus(userName, recommendNo));
+    System.out.println(likesService.countNumberLikes(recommendNo));
+    return likesService.countNumberLikes(recommendNo);
   }
 
   /**
@@ -42,7 +55,7 @@ public class LikesController {
    * @return 좋아요 개수를 숫자로 반환합니다.
    */
   @GetMapping("/{recommendNo}/likes")
-  public Response<Integer> returnNumberLikes(@PathVariable final Long recommendNo) {
+  public Response<Long> returnNumberLikes(@PathVariable final Long recommendNo) {
     return Response.success(likesService.countNumberLikes(recommendNo));
   }
 }
