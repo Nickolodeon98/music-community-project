1429e3d8eb7b047836519c5d2a7ed05e3a94360b
Nickolodeon98 <sjeon0730@gmail.com>
2023-02-14 22:00:15 +0900

    ✨feat: apply deletion of parameter memberEmail from methods related to fetching data from database to test and make testController concise

--- src/main/java/com/content_i_like/controller/TestController.java
+++ src/main/java/com/content_i_like/controller/TestController.java
@@ -32,53 +32,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Slf4j
 public class TestController {
 
-  private final TrackService trackService;
-
-  @Value("${spotify.client.id}")
-  private String CLIENT_ID;
-
-  @GetMapping("/tracks")
-  public String getTracks(@RequestParam String token) throws IOException {
-    trackService.createAllThreeTypesDB(token);
-
-    return "DB 저장이 완료되었습니다.";
-  }
-
-  @GetMapping("/token")
-  public ResponseEntity<?> requirePermission() {
-    HttpHeaders headers = new HttpHeaders();
-
-    String uri = "https://accounts.spotify.com/authorize?"
-        + String.format("client_id=%s&response_type=%s&redirect_uri=%s", CLIENT_ID,
-        "code", TrackEnum.REDIRECT_URI.getValue());
-
-    headers.setLocation(URI.create(uri));
-
-    return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
-  }
-
-  @GetMapping("")
-  public ResponseEntity<?> getAccessToken(@RequestParam String code)
-      throws JsonProcessingException {
-    HttpHeaders headers = new HttpHeaders();
-
-    String accessToken = trackService.spotifyAccessTokenGenerator(code);
-
-    String uri = "http://localhost:8080/api/v1/test/tracks?token=" + accessToken;
-
-    headers.setLocation(URI.create(uri));
-
-    return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
-  }
-
   @GetMapping("/thymeleaf")
-  public String testForThymeleafListModel(@ModelAttribute("trackRequestDto") TrackRequest trcKM) {
+  public String testForThymeleafListModel(@ModelAttribute("trackRequestDto") TrackRequest trackRequest) {
     log.info("로그입니다");
-    Track track = Track.builder().trackTitle(trcKM.getTrackTitle()).build();
-    log.info("titel:{}",trcKM.getTrackTitle());
+    Track track = Track.builder().trackTitle(trackRequest.getTrackTitle()).build();
+    log.info("title:{}",trackRequest.getTrackTitle());
     return "pages/search/tracks-search";
-
-
   }
-
 }
