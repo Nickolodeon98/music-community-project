1055573130a6b8571ed8d3d2aea33ba9ece053f0
lay423 <junpang486@gmail.com>
2023-02-14 17:45:01 +0900

    🚨fix: db 오류 해결

--- src/main/java/com/content_i_like/service/NotificationService.java
+++ src/main/java/com/content_i_like/service/NotificationService.java
@@ -56,30 +56,34 @@ public class NotificationService {
   public List<NotificationThymeleafResponse> getListBySlice(Slice<Notification> notifications) {
     List<NotificationThymeleafResponse> responseList = new ArrayList<>();
 
+    Member nullMember = Member.builder()
+        .nickName("")
+        .profileImgUrl("https://content-i-like.s3.ap-northeast-2.amazonaws.com/80c4f0a7-c4e0-44a5-85d6-315dc793fe28-profile.jpg")
+        .build();
+
     for (Notification notification : notifications) {
-      String commentContent;
-      String recommendTitle;
+      String commentContent="";
+      String recommendTitle="";
+      Member fromMember = nullMember;
 
-      Comment comment = commentRepository.findById(notification.getCommentNo()).orElse(null);
-      if (comment != null) {
-        commentContent = comment.getCommentContent();
-      } else {
-        commentContent = "삭제된 댓글";
+      if (notification.getCommentNo() != null) {
+        Comment comment = commentRepository.findById(notification.getCommentNo()).orElse(null);
+        if (comment != null) {
+          commentContent = comment.getCommentContent();
+        }
       }
 
-      Recommend recommend = recommendRepository.findById(notification.getRecommendNo())
-          .orElse(null);
-      if (recommend != null) {
-        recommendTitle = recommend.getRecommendTitle();
-      } else {
-        recommendTitle = "삭제된 게시글";
+      if (notification.getRecommendNo() != null) {
+        Recommend recommend = recommendRepository.findById(notification.getRecommendNo())
+            .orElse(null);
+        if (recommend != null) {
+          recommendTitle = recommend.getRecommendTitle();
+        }
       }
 
-      Member nullMember = Member.builder()
-          .nickName("탈퇴한 유저")
-          .profileImgUrl("https://content-i-like.s3.ap-northeast-2.amazonaws.com/80c4f0a7-c4e0-44a5-85d6-315dc793fe28-profile.jpg")
-          .build();
-      Member fromMember = memberRepository.findById(notification.getFromMemberNo()).orElse(nullMember);
+      if (notification.getFromMemberNo() != null) {
+        fromMember = memberRepository.findById(notification.getFromMemberNo()).orElse(nullMember);
+      }
 
       NotificationThymeleafResponse thymeleafResponse = NotificationThymeleafResponse.of(notification, fromMember,
           recommendTitle, commentContent);
