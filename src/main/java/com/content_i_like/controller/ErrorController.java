b69b4c396f7e41f7a96bc277c45200ab938704fa
chordpli <chordplaylist@gmail.com>
2023-02-15 01:47:43 +0900

    ✨feat($front): error controller

---
+++ src/main/java/com/content_i_like/controller/ErrorController.java
@@ -0,0 +1,20 @@
+package com.content_i_like.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
+@Controller
+@Slf4j
+public class ErrorController {
+
+  @GetMapping("/errors")
+  public String handleError() {
+    return "error";
+  }
+}
