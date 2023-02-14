00748d4882d07c4745a39c89ad84c40548430fa1
chordpli <chordplaylist@gmail.com>
2023-02-15 01:56:11 +0900

    ✨feat($front): error page

--- src/main/java/com/content_i_like/controller/ErrorController.java
+++ src/main/java/com/content_i_like/controller/ErrorController.java
@@ -1,10 +1,7 @@
 package com.content_i_like.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
