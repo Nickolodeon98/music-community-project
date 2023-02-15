d316f39c74fe8f57462cdfa5f524137c12f89b89
chordpli <chordplaylist@gmail.com>
2023-02-15 10:50:13 +0900

    ✨feat($front): 댓글 삭제 구현

--- src/main/java/com/content_i_like/domain/dto/comment/CommentResponse.java
+++ src/main/java/com/content_i_like/domain/dto/comment/CommentResponse.java
@@ -1,16 +1,36 @@
 package com.content_i_like.domain.dto.comment;
 
+import com.content_i_like.domain.entity.Comment;
+import com.content_i_like.domain.entity.Member;
+import com.content_i_like.domain.entity.Recommend;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Builder
 public class CommentResponse {
 
   private Long commentNo;
   private Long postNo;
   private String commentContent;
   private Long commentPoint;
+  private String profileImgUrl;
+  private String memberNickname;
+  private LocalDateTime createdAt;
+
+  public static CommentResponse of(Comment comment, Recommend post, Member member) {
+    return CommentResponse.builder()
+        .commentNo(comment.getCommentNo())
+        .postNo(post.getRecommendNo())
+        .commentContent(comment.getCommentContent())
+        .commentPoint(comment.getCommentPoint())
+        .profileImgUrl(member.getProfileImgUrl())
+        .memberNickname(member.getNickName())
+        .build();
+  }
 }
