fb155172424417fce7d62bb0677ee180d5efecfe
Nickolodeon98 <sjeon0730@gmail.com>
2023-03-07 18:25:49 +0900

    ✨feat: change predefined API method to set cookie for a value to be used after redirection
    - add an API method that redirects to API with parameters specifying which keyword to be used for searching

--- src/main/java/com/content_i_like/controller/restcontroller/TestRestController.java
+++ src/main/java/com/content_i_like/controller/restcontroller/TestRestController.java
@@ -3,6 +3,8 @@ package com.content_i_like.controller.restcontroller;
 import com.content_i_like.domain.enums.TrackEnum;
 import com.content_i_like.service.TrackService;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.net.URI;
 
-@RestController
 @RequestMapping("/api/v1/test")
 @RequiredArgsConstructor
 @Slf4j
@@ -26,6 +27,7 @@ public class TestRestController {
   private String CLIENT_ID;
 
   @GetMapping("/new/tracks")
+  @ResponseBody
   public String getNewTracks(@RequestParam String token) throws IOException {
     trackService.createAllThreeTypesDBAllUnique(token);
 
@@ -33,19 +35,32 @@ public class TestRestController {
   }
 
   @GetMapping("/token")
-  public ResponseEntity<?> requirePermission() {
+  @ResponseBody
+  public ResponseEntity<?> requirePermission(@RequestParam(required = false) String option,
+      @RequestParam(required = false) String keyword, HttpServletResponse httpServletResponse) {
     HttpHeaders headers = new HttpHeaders();
 
     String uri = "https://accounts.spotify.com/authorize?"
-        + String.format("client_id=%s&response_type=%s&redirect_uri=%s", CLIENT_ID,
-        "code", TrackEnum.REDIRECT_URI.getValue());
+        + String.format("client_id=%s&response_type=%s", CLIENT_ID, "code");
 
-    headers.setLocation(URI.create(uri));
+    URI redirectPath = URI.create(uri + "&redirect_uri=" + TrackEnum.REDIRECT_URI.getValue());
+
+    if (option.equals("unplanned")) {
+      Cookie cookie = new Cookie("keyword", keyword);
+      cookie.setMaxAge(3 * 24 * 60 * 60);
+      httpServletResponse.addCookie(cookie);
+
+      redirectPath
+          = URI.create(uri + "&redirect_uri=" + TrackEnum.SECOND_REDIRECT_URI.getValue());
+    }
+
+    headers.setLocation(redirectPath);
 
     return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
   }
 
   @GetMapping("")
+  @ResponseBody
   public ResponseEntity<?> getAccessToken(@RequestParam String code)
       throws JsonProcessingException {
     HttpHeaders headers = new HttpHeaders();
@@ -59,5 +74,21 @@ public class TestRestController {
     return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
   }
 
+  /* 이 때 세션에 저장된 쿠키의 값을 읽어들인다. */
+  @GetMapping("/demand")
+  @ResponseBody
+  public ResponseEntity<?> unPlannedCall(@RequestParam String code,
+      @CookieValue(value = "keyword", defaultValue = "love") String keyword)
+      throws JsonProcessingException {
+    HttpHeaders headers = new HttpHeaders();
 
+    String accessToken = trackService.spotifyAccessTokenGenerator(code);
+
+    String uri =
+        "http://localhost:8080/api/v1/search/tracks/demand?token=" + accessToken + "&keyword=" + keyword;
+
+    headers.setLocation(URI.create(uri));
+
+    return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
+  }
 }
