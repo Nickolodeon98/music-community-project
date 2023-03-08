e0634a5227990795c3784f4841fb4c372434b249
Nickolodeon98 <sjeon0730@gmail.com>
2023-03-08 16:02:11 +0900

    ✨feat: add classes implementing Fetch interface which parse Json data in customized way

---
+++ src/main/java/com/content_i_like/service/fetchoptions/EachAlbumFetch.java
@@ -0,0 +1,66 @@
+package com.content_i_like.service.fetchoptions;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class EachAlbumFetch extends AlbumFetch {
+
+  @Override
+  public String extractData(JsonNode root, int count) {
+    StringBuilder artistNames = new StringBuilder();
+    StringBuilder artistIds = new StringBuilder();
+
+    String albumName = "";
+    String albumImageUrl = "";
+    String artistName = "";
+    String albumId = "";
+    String albumReleaseDate = "";
+    String totalTracks = "";
+    String artistSpotifyId = "";
+
+    JsonNode artistNode = null;
+
+    int valuesCount = 0;
+
+    try {
+      albumName = root.get("tracks").get("items").get(count).get("album").get("name").asText();
+
+      /* 1번 인덱스에서 가져오는 이유는 1번이 height 300, width 300 이미지의 링크를 담고 있기 때문이다. */
+      albumImageUrl = root.get("tracks").get("items").get(count).get("album").get("images").get(1).get("url")
+          .asText();
+
+      albumId = root.get("tracks").get("items").get(count).get("album").get("id").asText();
+      albumReleaseDate = root.get("tracks").get("items").get(count).get("album").get("release_date").asText();
+      totalTracks = root.get("tracks").get("items").get(count).get("album").get("total_tracks").asText();
+
+
+      artistNode = root.get("tracks").get("items").get(count).get("artists");
+      if (artistNode.isArray()) {
+        valuesCount = artistNode.size();
+      }
+
+    } catch (NullPointerException e) {
+      log.warn("JsonNode 가 null 값입니다.");
+      return "";
+    }
+
+    for (int i = 0; i < valuesCount; i++) {
+      artistName = artistNode.get(i).get("name").asText();
+      artistSpotifyId = artistNode.get(i).get("id").asText();
+      artistNames.append(artistName);
+      artistIds.append(artistSpotifyId);
+      if (i != valuesCount - 1) {
+        artistNames.append(", ");
+        artistIds.append(", ");
+      }
+    }
+
+    if (albumName.equals("") || albumImageUrl.equals("") || artistNames.isEmpty() || albumId.equals("")
+        || albumReleaseDate.equals("") || totalTracks.equals(""))
+      return "";
+
+    return String.format("%s\n%s\n%s\n%s\n%s\n%s\n%s",
+        albumName, albumImageUrl, artistNames, albumId, albumReleaseDate, totalTracks, artistIds);
+  }
+}
